c066e1957cedb1fefbe616994f3d65e93bdf2c3b
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-04-16 02:25:17 +0530

    Remove thiserror dependency

--- src/lib.rs
+++ src/lib.rs
@@ -89,6 +89,13 @@ pub fn home_dir() -> Result<std::path::PathBuf, HomeDirError> {
 }
 
 /// This error occurs when the home directory cannot be located.
-#[derive(Debug, thiserror::Error)]
-#[error("could not locate home directory")]
+#[derive(Debug)]
 pub struct HomeDirError;
+
+impl std::fmt::Display for HomeDirError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "could not locate home directory")
+    }
+}
+
+impl std::error::Error for HomeDirError {}
